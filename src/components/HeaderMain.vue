e57d8a367098b844430a35ee29725cd119a5d892
Thomricco <tommasoricco@hotmail.it>
2022-02-08 18:14:49 +0100

    fine header main

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -4,6 +4,12 @@
             <p>Do You Have A Construction Project We Can Help With?</p>
             <div class="bottone">GET A FREE QUOTE</div>
         </div>
+        <div class="contain">
+            <p>Special In Modern Construction</p>
+            <div class="bordo"></div>
+            <span class="lorem">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum rerum nostrum animi facere quisquam nihil modi et, optio nesciunt odio sapiente repudiandae placeat aspernatur consectetur</span>
+        </div>
+        
     </div>
 </template>
 
@@ -32,29 +38,59 @@ export default {
         flex-direction: column;
         justify-content: center;
         align-items: center;
-            p {
-                height: 60px;
+        p {
+            height: 60px;
+            color: $color-silver-dark;
+        }
+        .bottone {
+            width: 150px;
+            height: 30px;
+            text-align: center;
+            line-height:  20px;
+            padding: 5px;
+            font-size: 12px;
+            background-color: $color-tuatara;
+            color: $color-white;
+            margin-left: 15px;
+            border: 1px $color-white solid;
+            &:hover {
+                cursor: pointer;
+                background-color: $color-white;
+                color: $color-silver-dark;
+                border: 1px $color-silver-dark solid;
+                
             }
-            .bottone {
-                width: 150px;
-                height: 30px;
-                text-align: center;
-                line-height:  20px;
-                padding: 5px;
-                font-size: 12px;
-                background-color: $color-tuatara;
-                color: $color-white;
-                margin-left: 15px;
-                border: 1px $color-white solid;
-                &:hover {
-                    cursor: pointer;
-                    background-color: $color-white;
-                    color: $color-silver-dark;
-                    border: 1px $color-silver-dark solid;
-                    
-                }
-            }
+        }
+
     }
+    .contain {
+        background-color: $color-white;
+        margin: 0 auto;
+        height: 300px;
+        text-align: center;
+        font-size: 20px;
+        line-height: 40px;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        p {
+            color: $color-silver-dark;
+        }
+        .bordo {
+            width: 150px;
+            border: 1px $color-bright-sun-text solid;
+        }
+        .lorem {
+            color: $color-silver-dark;
+            font-size: 10px;
+            width: 600px;
+            height: 60px;
+
+        }
+    }
+
 }
 
 </style>
